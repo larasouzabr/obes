0515bdaa9671cd7baec14d8c4fa24efcec41d39c
Lara Souza <laragabriellysouzabatista@gmail.com>
2023-04-27 12:15:42 -0300

    aaaaaaaaaaa

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
+  <nav-bar></nav-bar>
   <router-view />
 </template>
 
 <script>
-/* import NavBar from "./components/NavBar.vue"; */
+ import NavBar from "./components/NavBar.vue"; 
 
 export default {
   name: "App",
   components: {
-    /* NavBar */
+    NavBar 
   },
 };
 </script>
